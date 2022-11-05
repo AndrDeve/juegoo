e136907132e347e74dc12b9240f81c0d24ca8859
Andrés <marlon_rivera95@hotmail.com>
2022-11-05 11:05:33 -0500

    subida de pagina11111

--- funciones.js
+++ funciones.js
@@ -7,7 +7,7 @@ window.onload = function () {
 		ctx = canvas.getContext("2d");
 		if (ctx) {
 			x = canvas.width / 2;
-			mensaje("INVASORES");
+			mensaje("JUEGUELO");
 			imgNave = new Image();
 			imgOvni = new Image();
 			imgOvni.src = "imagenes/ovni.png";
